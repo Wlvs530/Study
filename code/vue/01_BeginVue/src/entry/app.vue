7c72e7822a79ce6611070280847a83a7e927fbd9
Wlvs530 <11096732@qq.com>
2019-08-23 19:47:54 +0800

    update app.vue

---
+++ code/vue/01_BeginVue/src/entry/app.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        First Vue App ! <br>
+        {{ Message }} <br>
+        <input v-model="Message" />
+        <ul>
+            <li v-for="Language in Languages" >
+                {{ Language.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            Message : 'Hello, World !',
+            Languages : [{ name : 'Java'},{name : 'C#'},{name : 'C'}]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>>
